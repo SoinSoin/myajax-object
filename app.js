705c64c089d9da79e512ead770421ad8d63d9949
SoinSoin <vincent.retore@gmail.com>
2018-09-09 15:09:49 +0200

    ok

--- app.js
+++ app.js
@@ -6,43 +6,54 @@ class Ajax {
     get parameters() {
         return this.setParameters;
     }
-    setParameters(param = {
-        methode: null,
-        url: null,
-        data: {
-            key: null,
-        }
-    }) {
+    setParameters(param) {
+        param.parse;
+        param.methode;
+        param.url;
+        param.data.key;
+        param.data.value;
+        // define option parameters of XML objects 
         this.param = param;
     }
-    get bind() {
-        return this.setBind;
+    get connect() {
+        return this.setConnect;
     }
-    setBind() {
+    setConnect() {
         this.objHTTP.open(this.param.methode, this.param.url, true);
-        this.onload = this.value;
-        this.objHTTP.send(null);
+        this.objHTTP.setRequestHeader("Access-Control-Allow-Origin", "*");
+    }
+    get res() {
+        return this.setRes;
+    }
+    setRes() {
+        this.objHTTP.onload = this.value;
+        // this.objHTTP.send(this.param.key + '=' + this.param.value);
     }
     get value() {
-        return this.setValue;
-    }
-    setValue() {
         try {
-            console.log(this.responseText);
+            this.setValue;
+
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
-
+    setValue() {
+        var data = JSON.parse(this.responseText);
+        console.log(data)
+        return data;
+    }
 }
 
 var test1 = new Ajax();
 test1.parameters({
+    parse: 'json',
     methode: 'GET',
     url: 'https://randomuser.me/api/',
     data: {
-        'cle': 'valeur',
+        // key: 'mykey',
+        // value: 'value',
     }
 })
-
-test1.bind()
+test1.connect();
+var myData = test1.res();
+// console.log
